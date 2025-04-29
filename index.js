b9bc89236c8f6e9caca98def88fda4a071862701
Kirti Bhosale <bhosalekirti2002@gmail.com>
2025-04-29 20:12:43 +0530

    new 01

--- index.js
+++ index.js
@@ -23,16 +23,22 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+
+app.get("/", () => {
+  res.status(200).json({ message: "Welcome to panini8" });
+});
 
 connectDB();
 // ✅ For local dev — comment this out if deploying on Vercel

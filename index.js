3b83378d8efa5c57ecaa41442e2d12b6d4f1ccf9
Kirti Bhosale <bhosalekirti2002@gmail.com>
2025-04-29 20:08:38 +0530

    new 01

--- index.js
+++ index.js
@@ -10,10 +10,24 @@ const commentRoutes = require("./routes/commentRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(cors({
   origin: ['http://localhost:5173'],
   credentials: true
 }));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);

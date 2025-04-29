61407da94d2a8dc7ca5fc0c8cff1f816628ea9be
Kirti Bhosale <bhosalekirti2002@gmail.com>
2025-04-29 19:56:11 +0530

    updated

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const commentRoutes = require("./routes/commentRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173/', 
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);

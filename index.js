d13cff0da411cc95cc4905c5b389bbcbc9299341
Kirti Bhosale <bhosalekirti2002@gmail.com>
2025-04-29 20:20:05 +0530

    new 01

--- index.js
+++ index.js
@@ -10,23 +10,12 @@ const commentRoutes = require("./routes/commentRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+    allowedHeaders: ["Content-Type", "Authorization"],   
   })
 );
 
@@ -41,10 +30,8 @@ app.get("/", () => {
 });
 
 connectDB();
-// ✅ For local dev — comment this out if deploying on Vercel
 // app.listen(PORT, () => {
 //   console.log(`Server running on ${PORT}`);
 // });
 
-// ✅ For Vercel — uncomment this if deploying on Vercel
 module.exports = app;
